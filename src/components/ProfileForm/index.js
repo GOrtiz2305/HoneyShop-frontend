c638f5266685bb0236c7c73146c12c3dce5298fa
GOrtiz2305 <160190916+GOrtiz2305@users.noreply.github.com>
2024-10-03 17:55:22 -0600

    Profile

--- src/components/ProfileForm/index.js
+++ src/components/ProfileForm/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
+import { URL } from "../../config";
+import axios from 'axios';
 
 function parseJwt(token) {
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
     var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
-  
+
     return JSON.parse(jsonPayload);
 }
 
@@ -21,6 +23,30 @@ class ProfileForm extends Component {
         error: {}
     }
 
+    componentDidMount() {
+        this.fetchUserData(); // Fetch user data on component mount
+    }
+
+    fetchUserData = async () => {
+        this.setState({ isLoading: true }); // Set loading state to true
+
+        try {
+            const response = await axios.get(URL + 'clients/user/' + parseJwt(localStorage.getItem('token')).user.id); // Fetch user data
+            const userData = response.data; // Access user data from response
+
+            this.setState({
+                names: userData.names,
+                last_names: userData.last_names,
+                address: userData.address,
+                phone: userData.phone,
+            });
+        } catch (error) {
+            console.error('Error fetching user data:', error);
+            // Handle errors appropriately, e.g., display an error message
+        } finally {
+            this.setState({ isLoading: false }); // Set loading state to false
+        }
+    };
 
     changeHandler = (e) => {
         const error = this.state.error;
@@ -70,37 +96,37 @@ class ProfileForm extends Component {
         }
     }
 
-    render(){
+    render() {
         const { names,
             last_names,
             address,
             phone,
             error } = this.state;
 
-        return(
+        return (
             <form onSubmit={this.subimtHandler} className="form">
                 <div className="row">
                     <div className="col-lg-6 col-md-6 col-12">
                         <div className="form-field">
-                            <input value={names} onChange={this.changeHandler} type="text" name="names" placeholder="Names"/>
+                            <input value={names} onChange={this.changeHandler} type="text" name="names" placeholder="Names" />
                             <p>{error.names ? error.names : ''}</p>
                         </div>
                     </div>
                     <div className="col-lg-6 col-md-6 col-12">
                         <div className="form-field">
-                            <input value={last_names} onChange={this.changeHandler} type="text" name="last_names" placeholder="Last names"/>
+                            <input value={last_names} onChange={this.changeHandler} type="text" name="last_names" placeholder="Last names" />
                             <p>{error.last_names ? error.last_names : ''}</p>
                         </div>
                     </div>
                     <div className="col-lg-6 col-md-6 col-12">
                         <div className="form-field">
-                            <input onChange={this.changeHandler} value={address} type="text" name="address" placeholder="Address"/>
+                            <input onChange={this.changeHandler} value={address} type="text" name="address" placeholder="Address" />
                             <p>{error.address ? error.address : ''}</p>
                         </div>
                     </div>
                     <div className="col-lg-6 col-md-6 col-12">
                         <div className="form-field">
-                            <input onChange={this.changeHandler} value={phone} type="text" name="phone" placeholder="Phone"/>
+                            <input onChange={this.changeHandler} value={phone} type="text" name="phone" placeholder="Phone" />
                             <p>{error.phone ? error.phone : ''}</p>
                         </div>
                     </div>
@@ -115,4 +141,4 @@ class ProfileForm extends Component {
     }
 
 }
-export default  ProfileForm;
+export default ProfileForm;
